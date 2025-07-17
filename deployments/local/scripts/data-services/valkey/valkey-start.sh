97a618f344aa1cae8c87e503d78e7f522859707e
ggreen <gregoryg@vmware.com>
2025-07-17 07:51:29 -0400

    adding pom fix

--- deployments/local/scripts/data-services/valkey/valkey-start.sh
+++ deployments/local/scripts/data-services/valkey/valkey-start.sh
@@ -1,6 +1,6 @@
 podman network create tanzu
 
-podman run --rm -d --name valkey-server \
+podman run --rm -it --name valkey-server \
     -e ALLOW_EMPTY_PASSWORD=yes \
     --network tanzu  -p 6379:6379 \
     bitnami/valkey:8.0.2
@@ -9,4 +9,4 @@ podman run --rm -d --name valkey-server \
 #podman run -it --rm \
 #        -e VALKEY_PRIMARY_HOST=valkey-server \
 #        --network tanzu  -p 26379:26379 \
-#        bitnami/valkey-sentinel:8.0.2
+#        bitnami/valkey-sentinel:8.0.
