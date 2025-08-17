4198813bcdd8f3aad3a994c1d2375fa7638f5887
ggreen <gregoryg@vmware.com>
2025-08-16 21:13:29 -0400

    adding jdbc console improvements

--- deployments/cloud/cloudFoundry/apps/jdbc-sql-console-app/cf-push.sh
+++ deployments/cloud/cloudFoundry/apps/jdbc-sql-console-app/cf-push.sh
@@ -8,4 +8,4 @@
 #--------------------
 # Push Applications
 # jdbc-sql-console-app
-cf push jdbc-sql-console-app -f deployments/cloud/cloudFoundry/apps/jdbc-sql-console-app/jdbc-sql-console-app.yaml -p applications/jdbc-sql-console-app/target/jdbc-sql-console-app-0.0.4-SNAPSHOT.jar
+cf push jdbc-sql-console-app -f deployments/cloud/cloudFoundry/apps/jdbc-sql-console-app/jdbc-sql-console-app.yaml -b java_buildpack_offline -p applications/jdbc-sql-console-app/target/jdbc-sql-console-app-0.0.4.jar
